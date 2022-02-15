8de9f7d8adc27842c8a616a47754763c06ea720c
Dragonyu <2568698669@qq.com>
2022-02-15 19:02:26 +0800

    second

---
+++ test2.c
@@ -0,0 +1,19 @@
+//#include<stdio.h>
+//int min(int x, int y)
+//{
+//	if (x > y)
+//	{
+//		return y;
+//	}
+//	else
+//		return x;
+//}
+//int main()
+//{
+//	int a, b, c;
+//	printf("input=");
+//	scanf_s("%d%d%d", &a, &b, &c);
+//	int num=min(min(a, b), c);
+//	printf("smallest=%d\n", num);
+//	return 0;
+//}
